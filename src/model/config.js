eca87f84f7c79a29a3cfd06b88d9f32965a4eb7c
florent etheve <fetheve2@gmail.com>
2022-08-15 10:56:39 +0400

    sequelize request timeout

--- src/model/config.js
+++ src/model/config.js
@@ -2,6 +2,7 @@ var Sequelize = require('sequelize');
 var sequelize;
 //const sequelize=new Sequelize({dialect:"mysql"});
 //db=(typeof process.env.DB_DATABASE!="undfined") ? process.env.DB_CONNECTION+"://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_HOST+":"+process.env.DB_PORT+"/"+process.env.DB_DATABASE :"mysql://root:root@localhost:3306/vetement"
+
 if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
     sequelize = new Sequelize(process.env.DATABASE_URL, {
